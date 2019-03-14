3c52c7e55304b80faa5540b14801227aad0e4760
Guillaume Gomez <guillaume1.gomez@gmail.com>
2019-03-14 15:36:55 +0100

    Change how requests are handled

--- server-src/server.js
+++ server-src/server.js
@@ -9,7 +9,12 @@ const utils = require('./utils.js');
 var DOC_UI_RUNS = {};
 
 function get_status(response) {
-    response.end('All good here!');
+    response.write('<html>');
+    response.write('<body>');
+    response.write("<h1>Everything's good here!</h1>");
+    response.write('</body>');
+    response.write('</html>');
+    response.end();
 }
 
 function restart(response, request, server) {
@@ -55,8 +60,7 @@ function uninstallRustdoc(id) {
     }
 }
 
-// https://developer.github.com/v3/activity/events/types/#issuecommentevent
-function github_event(response, request, server) {
+function parseData(response, request, server) {
     let body = [];
 
     request.on('error', (err) => {
@@ -64,109 +68,123 @@ function github_event(response, request, server) {
     }).on('data', (chunk) => {
         body.push(chunk);
     }).on('end', () => {
-        try {
-            let content = JSON.parse(Buffer.concat(body).toString());
+        let contentType = res.headers['content-type'];
 
-            if (content['action'] === 'deleted') {
-                return;
-            }
-
-            // If we received the message that the rustdoc binary is ready, we can start tests!
-            if (DOC_UI_RUNS[content['issue']['url']] === false) {
-                var id = null;
-                const lines = content['comment']['body'].split('\n');
-
-                for (var x = 0; x < lines.length; ++x) {
-                    if (lines[x].indexOf(" Try build successful - ") !== -1 &&
-                            x + 1 < lines.length &&
-                            lines[x + 1].startsWith("Build commit: ")) {
-                        id = lines[x + 1].split(' '); // Less chances to update this in case the
-                                                      // message is updated.
-                        id = id[id.length - 1];
-                        break;
-                    }
-                }
-                if (id !== null) {
-                    var ret = installRustdoc(id);
-                    if (ret !== true) {
-                        response.statusCode = 200;
-                        response.end("An error occurred:\n```text\n" + ret + "\n````");
-                        return;
-                    }
-                    tester.runTests(["", "", "rustdoc", id]).then(x => {
-                        var [output, error_code] = x;
-                        response.statusCode = 200;
-                        if (error_code > 0) {
-                            var failure = "failure";
-                            if (error_code > 1) {
-                                failure = "failures";
-                            }
-                            response.end("Rustdoc-UI tests failed (" + error_code + " " + failure +
-                                         ")...\n```text\n" + output + "\n```");
-                        } else {
-                            response.end("Rustdoc-UI tests passed!\n```text\n" + output + "\n```");
-                        }
-
-                        // cleanup part
-                        DOC_UI_RUNS[content['issue']['url']] = undefined;
-                        uninstallRustdoc(id);
-                    }).catch(err => {
-                        response.statusCode = 200;
-                        response.end("A test error occurred:\n```text\n" + err + "\n```");
-
-                        // cleanup part
-                        DOC_UI_RUNS[content['issue']['url']] = undefined;
-                        uninstallRustdoc(id);
-                    });
-                    return;
-                }
-            }
-
-            let msg = content['comment']['body'].split("\n");
-            let run_doc_ui = false;
-            let need_restart = false;
-            let need_update = false;
-            for (let i = 0; i < msg.length; ++i) {
-                if (line.trim().startsWith("@" + config.BOT_NAME) === false) {
-                    continue;
-                }
-                let parts = line.split(" ").filter(w => w.length > 0).slice(1);
-                for (var x = 0; x < parts.length; ++x) {
-                    let cmd = parts[x].toLowerCase();
-                    if (cmd === "run-doc-ui") {
-                        run_doc_ui = true;
-                    } else if (cmd === "restart") {
-                        need_restart = true;
-                    } else if (cmd === "update") {
-                        need_update = true;
-                    } else {
-                        // we ignore the rest.
-                    }
-                }
-            }
-            if ((need_restart === true || run_doc_ui === true || need_update === true) &&
-                    check_rights(content['comment']['user']['login']) === false) {
-                console.log('github_event: missing rights for ' + content['comment']['user']['login']);
-                return;
-            }
-            if (need_update === true) {
-                utils.updateRepository();
-            }
-            if (need_restart === true) {
-                restart(response, request, server);
-            }
-            if (run_doc_ui === true) {
-                // We wait for the rustdoc build to end before trying to get it.
-                DOC_UI_RUNS[content['issue']['url']] = false;
-            }
-
-            response.statusCode = 200;
-            response.end();
-        } catch (e) {
-            console.error('github_event: ', e);
-            response.end("Invalid github data");
+        if (contentType === "application/json") {
+            return github_event(response, request, server, body);
+        } else {
+            return get_status(response);
         }
     });
+}
+
+// https://developer.github.com/v3/activity/events/types/#issuecommentevent
+function github_event(response, request, server, body) {
+    try {
+        let content = JSON.parse(Buffer.concat(body).toString());
+
+        if (content['action'] === 'deleted') {
+            response.end();
+            return;
+        }
+
+        res.setHeader('Content-Type', 'application/json');
+
+        // If we received the message that the rustdoc binary is ready, we can start tests!
+        if (DOC_UI_RUNS[content['issue']['url']] === false) {
+            let id = null;
+            const lines = content['comment']['body'].split('\n');
+
+            for (let x = 0; x < lines.length; ++x) {
+                if (lines[x].indexOf(" Try build successful - ") !== -1 &&
+                        x + 1 < lines.length &&
+                        lines[x + 1].startsWith("Build commit: ")) {
+                    id = lines[x + 1].split(' '); // Less chances to update this in case the
+                                                  // message is updated.
+                    id = id[id.length - 1];
+                    break;
+                }
+            }
+            if (id !== null) {
+                let ret = installRustdoc(id);
+                if (ret !== true) {
+                    response.statusCode = 200;
+                    response.end("An error occurred:\n```text\n" + ret + "\n````");
+                    return;
+                }
+                tester.runTests(["", "", "rustdoc", id]).then(x => {
+                    let [output, error_code] = x;
+                    response.statusCode = 200;
+                    if (error_code > 0) {
+                        let failure = "failure";
+                        if (error_code > 1) {
+                            failure = "failures";
+                        }
+                        response.end("Rustdoc-UI tests failed (" + error_code + " " + failure +
+                                     ")...\n```text\n" + output + "\n```");
+                    } else {
+                        response.end("Rustdoc-UI tests passed!\n```text\n" + output + "\n```");
+                    }
+
+                    // cleanup part
+                    DOC_UI_RUNS[content['issue']['url']] = undefined;
+                    uninstallRustdoc(id);
+                }).catch(err => {
+                    response.statusCode = 200;
+                    response.end("A test error occurred:\n```text\n" + err + "\n```");
+
+                    // cleanup part
+                    DOC_UI_RUNS[content['issue']['url']] = undefined;
+                    uninstallRustdoc(id);
+                });
+                return;
+            }
+        }
+
+        let msg = content['comment']['body'].split("\n");
+        let run_doc_ui = false;
+        let need_restart = false;
+        let need_update = false;
+        for (let i = 0; i < msg.length; ++i) {
+            if (line.trim().startsWith("@" + config.BOT_NAME) === false) {
+                continue;
+            }
+            let parts = line.split(" ").filter(w => w.length > 0).slice(1);
+            for (var x = 0; x < parts.length; ++x) {
+                let cmd = parts[x].toLowerCase();
+                if (cmd === "run-doc-ui") {
+                    run_doc_ui = true;
+                } else if (cmd === "restart") {
+                    need_restart = true;
+                } else if (cmd === "update") {
+                    need_update = true;
+                } else {
+                    // we ignore the rest.
+                }
+            }
+        }
+        if ((need_restart === true || run_doc_ui === true || need_update === true) &&
+                check_rights(content['comment']['user']['login']) === false) {
+            console.log('github_event: missing rights for ' + content['comment']['user']['login']);
+            return;
+        }
+        if (need_update === true) {
+            utils.updateRepository();
+        }
+        if (need_restart === true) {
+            restart(response, request, server);
+        }
+        if (run_doc_ui === true) {
+            // We wait for the rustdoc build to end before trying to get it.
+            DOC_UI_RUNS[content['issue']['url']] = false;
+        }
+
+        response.statusCode = 200;
+        response.end();
+    } catch (e) {
+        console.error('github_event: ', e);
+        response.end("Invalid github data");
+    }
 }
 
 function readySubmodule(submodule_path) {
@@ -190,8 +208,8 @@ readySubmodule(utils.getCurrentDir() + "rustup-toolchain-install-master");
 
 const URLS = {
     '/status': get_status,
-    '/restart': restart,
-    '/github': github_event,
+    '/': parseData,
+    '': parseData,
 };
 
 var server = http.createServer((request, response) => {
